276b55c311b8f570bed467f4113362a40419eea3
supri0343 <63761987+supri0343@users.noreply.github.com>
2023-04-03 14:54:01 +0700

    enhance-aval-with-fasilitas

--- src/modules/customs-report/scrap/index.js
+++ src/modules/customs-report/scrap/index.js
@@ -1,7 +1,7 @@
 export class Index {
     configureRouter(config, router) {
         config.map([
-            { route: ['', 'list'], moduleId: './list', name: 'list', nav: true, title: 'List: Laporan Pertanggungjawaban Barang Reject dan Scrap' }
+            { route: ['', 'list'], moduleId: './list', name: 'list', nav: true, title: 'List: Laporan Penyelesaian Waste / Scrap' }
         ]);
         this.router = router;
     }
